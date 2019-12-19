de4365e3ce6b469abd164ae9fb3a0019c89f793b
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-19 15:14:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,12 @@
 #numbers = [1,2,3]
 
-def square_array(array)  # defines method named square_array, takes array parameter
+# def square_array(array)  # defines method named square_array, takes array parameter
 
-i = 0                 # starting point is 0
-while array[i] do    # while in the i index of the array
+# i = 0                 # starting point is 0
+# while array[i] do    # while in the i index of the array
   
-p array[i]**2         # prints value and returns it, for index i squared
-i += 1               # adds 1 to the counter to start the loop again
+# p array[i]**2         # prints value and returns it, for index i squared
+# i += 1               # adds 1 to the counter to start the loop again
 
-end
-end
+# end
+# end
